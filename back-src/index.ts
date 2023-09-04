57d74fb0fdb7a5f1935e31c386abdbcabe1ef7ef
nicolascasmuz <nicolascasmuz@hotmail.com>
2023-09-04 19:09:21 -0300

    commit-003

--- back-src/index.ts
+++ back-src/index.ts
@@ -255,7 +255,7 @@ app.post("/send-mail", async (req, res) => {
   });
 });
 
-app.use(express.static("dist"));
+app.use(express.static(path.join(__dirname, "../dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));

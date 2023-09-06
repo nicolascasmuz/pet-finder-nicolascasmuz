afaa4af14187c9603adb516d728e13072d911744
nicolascasmuz <nicolascasmuz@hotmail.com>
2023-09-05 22:32:33 -0300

    commit-014

--- back-src/lib/algolia.ts
+++ back-src/lib/algolia.ts
@@ -4,6 +4,7 @@ const client = algoliasearch(
   process.env.ALGOLIA_APPID,
   process.env.ALGOLIA_ADMINAPIKEY
 );
+
 const missingPetsIndex = client.initIndex("missingpets");
 const profilesIndex = client.initIndex("profiles");
 
